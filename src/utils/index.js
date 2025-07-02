f51c42849d587397ccbaac29f7b25fef55cde035
shubh2304 <shubh230405@gmail.com>
2025-07-02 14:52:20 +0530

    Changes

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import connectDB from "../db/index.js";
 
 dotenv.config({
     path: "./.env"
